1b8497924e334e3c227159d8e05373e29b1ad4cf
marliseborba <borba.marlise@gmail.com>
2023-08-08 13:51:06 -0300

    Registers InvoiceService in dependency injection system

--- Expenses/Program.cs
+++ Expenses/Program.cs
@@ -1,7 +1,10 @@
+using Expenses.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<InvoiceService>();
 
 var app = builder.Build();
 
@@ -22,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Upload}/{id?}");
+    pattern: "{controller=Invoice}/{action=Index}/{id?}");
 
 app.Run();
