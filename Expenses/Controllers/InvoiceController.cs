0b874eab945694231f7acc3b5810b3f65844bc03
marliseborba <borba.marlise@gmail.com>
2023-08-08 13:45:48 -0300

    Returns InvoiceViewModel in Index action in InvoiceController

--- Expenses/Controllers/InvoiceController.cs
+++ Expenses/Controllers/InvoiceController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Expenses.Services;
+using Expenses.Models.ViewModels;
 
 namespace Expenses.Controllers
 {
     public class InvoiceController : Controller
     {
+        private readonly InvoiceService _invoiceService;
+
+        public InvoiceController(InvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
         public IActionResult Index()
         {
-            
-            return View();
+            var viewModel = new InvoiceViewModel { Invoice = InvoiceService.Upload() };
+            return View(viewModel);
         }
 
 
