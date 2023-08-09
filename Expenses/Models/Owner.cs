07989e9797dc8746d55f65c9728391ea508fc935
marliseborba <borba.marlise@gmail.com>
2023-08-09 18:30:45 -0300

    Creates Owner entity

---
+++ Expenses/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace Expenses.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+    }
+}
