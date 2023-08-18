69846a5b13e502dc7105f028b816234985c38e3c
marliseborba <borba.marlise@gmail.com>
2023-08-18 02:20:35 -0300

    Creates Category Create and Edit controllers, services and views

--- Expenses/Controllers/CategoriesController.cs
+++ Expenses/Controllers/CategoriesController.cs
@@ -1,6 +1,9 @@
 ﻿using Expenses.Data;
+using Expenses.Models;
+using Expenses.Models.ViewModels;
 using Expenses.Services;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Expenses.Controllers
 {
@@ -9,18 +12,70 @@ namespace Expenses.Controllers
         public readonly ExpensesContext _context;
         private readonly SeedingService _seedService;
         private readonly CategoryService _categoryService;
+        private readonly MovementService _movementService;
 
-        public CategoriesController(ExpensesContext expensesContext, SeedingService seedService, CategoryService category)
+        public CategoriesController(ExpensesContext expensesContext, SeedingService seedService, CategoryService categoryService, MovementService movementService)
         {
             _context = expensesContext;
             _seedService = seedService;
-            _categoryService = category;
+            _categoryService = categoryService;
+            _movementService = movementService;
         }
 
         public IActionResult Index()
         {
             //_seedService.Seed();
-            return View(_context.Category.ToList());
+            return View(_categoryService.GetCategories().ToList());
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category) 
+        {
+            _categoryService.Insert(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não fornecido" });
+            }
+            var obj = _categoryService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado" });
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            if (id != category.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id incompatível" });
+            }
+            try
+            {
+                _categoryService.Update(category);
+                _movementService.UpdateEstablishments();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
     }
 }
--- Expenses/Services/CategoryService.cs
+++ Expenses/Services/CategoryService.cs
@@ -1,5 +1,6 @@
 ﻿using Expenses.Data;
 using Expenses.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expenses.Services
 {
@@ -12,9 +13,42 @@ namespace Expenses.Services
             _context = context;
         }
 
-        public List<Category> GetCategories() 
+        public List<Category> GetCategories()
         {
             return _context.Category.ToList();
+        }
+
+        public void Insert(Category category)
+        {
+            _context.Add(category);
+            _context.SaveChanges();
+        }
+
+        public Category Update(Category category)
+        {
+            bool hasAny = _context.Category.Any(x => x.Id == category.Id);
+            if (!hasAny)
+            {
+                //throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return category;
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                //throw new DbConcurrencyException(e.Message);
+            }
+            return null;
+        }
+
+        public Category FindById(int id)
+        {
+            return _context.Category
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
         }
     }
 }
