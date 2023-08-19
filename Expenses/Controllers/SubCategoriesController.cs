a8a5f5ba5e77d54e8f2dba2e049512ddf68b51e6
marliseborba <borba.marlise@gmail.com>
2023-08-18 23:34:54 -0300

    Creates SubCategory Index, Create and Edit controllers, services and views

---
+++ Expenses/Controllers/SubCategoriesController.cs
@@ -0,0 +1,77 @@
+﻿using Expenses.Data;
+using Expenses.Models;
+using Expenses.Services;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Expenses.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ExpensesContext _context;
+        private readonly SubCategoryService _subCategoryService;
+        private readonly MovementService _movementService;
+
+        public SubCategoriesController(ExpensesContext context, SubCategoryService subCategoryService, MovementService movementService)
+        {
+            _context = context;
+            _subCategoryService = subCategoryService;
+            _movementService = movementService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_subCategoryService.GetSubCategories().ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SubCategory subCategory)
+        {
+            _subCategoryService.Insert(subCategory);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não fornecido" });
+            }
+            var obj = _subCategoryService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado" });
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SubCategory subCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(subCategory);
+            }
+            if (id != subCategory.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id incompatível" });
+            }
+            try
+            {
+                _subCategoryService.Update(subCategory);
+                _movementService.UpdateEstablishments();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+    }
+}
--- Expenses/Models/SubCategory.cs
+++ Expenses/Models/SubCategory.cs
@@ -7,8 +7,8 @@ namespace Expenses.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public Category Category { get; set; }
-        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int? CategoryId { get; set; }
         public ICollection<Establishment> Establishments { get; set; } = new List<Establishment>();
 
         public SubCategory() { }
@@ -16,6 +16,12 @@ namespace Expenses.Models
         public SubCategory(int id, string name, Category category)
         {
             Id = id;
+            Name = name;
+            Category = category;
+        }
+
+        public SubCategory(string name, Category category)
+        {
             Name = name;
             Category = category;
         }
