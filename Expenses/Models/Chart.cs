9d0e6443bc358284c886e8172d42a5dad376404a
marliseborba <borba.marlise@gmail.com>
2023-09-01 19:25:48 -0300

    Full object Chart

--- Expenses/Models/Chart.cs
+++ Expenses/Models/Chart.cs
@@ -1,23 +1,16 @@
 ﻿using Expenses.Services;
+using Newtonsoft.Json;
 
 namespace Expenses.Models
 {
 	public class Chart
 	{
-		public string title { get; set; }
-		public AxisY axisY { get; set; }
-		public AxisX axisX { get; set; }
-		public ICollection<DataSerie> data { get; set; } = new List<DataSerie>();
+        public ICollection<DataSerie> data { get; set; } = new List<DataSerie>();
 
 
 		public Chart()
 		{
-			title = "Gráfico Teste";
-			axisX = new AxisX();
-			axisY = new AxisY();
-			axisX.valueFormatString = "MMM";
-			axisY.valueFormatString = "#.###,##";
-			axisY.prefix = "R$";
-		}
+
+        }
 	}
 }
--- Expenses/Models/ViewModels/ChartViewModel.cs
+++ Expenses/Models/ViewModels/ChartViewModel.cs
@@ -1,17 +1,21 @@
-﻿namespace Expenses.Models.ViewModels
+﻿using Newtonsoft.Json;
+
+namespace Expenses.Models.ViewModels
 {
     public class ChartViewModel
     {
         public string ChartJ { get; set; }
-
-        public Chart Chart { get; set; }
-
+		public string axisYJ { get; set; }
+		public string axisXJ { get; set; }
+		public Chart Chart { get; set; }
         public string DataSeriesJ { get; set; }
         public List<DataSerie> DataSeries { get; set; } = new List<DataSerie>();
         public string DataPoints { get; set; }
 
         public ChartViewModel() 
         {
+			//axisYJ = JsonConvert.SerializeObject(Chart.axisY);
+			//axisXJ = JsonConvert.SerializeObject(Chart.axisX);
 		}
     }
 }
